38985a041dd701cc122220e96ed5c1ad99a7103e
kubistmi <kubistmi@gmail.com>
2017-12-10 22:57:39 +0100

    Seminar 4 completed.

--- w4/seminar4.R
+++ w4/seminar4.R
@@ -17,9 +17,11 @@ url <- "https://archive.ics.uci.edu/ml/machine-learning-databases/00292/Wholesal
 wholesale <- fread(url)
 rm(url)
 
+## data overview
 str(wholesale)
 summary(wholesale)
 
+## unique region and channel values
 wholesale %>%
       select(Channel, Region) %>% 
       map(unique)
@@ -28,7 +30,7 @@ wholesale %>%
 table(wholesale[,1:2])
 ggpairs(wholesale[,-1:-2])
 
-## LOG SCALLING
+## log scalling
 wholesale %>% 
       select(Fresh : Delicassen) %>% 
       map_df(log) %>% 
@@ -49,7 +51,7 @@ wholeLg[,
         .SDcols = colnames(wholesale)[-1:-2]
         ]
 
-# OUTLIERS BEFORE AND AFTER
+## boxplots of original and scalled data
 for (i in colnames(wholesale)[-1:-2]) {
       boxplot(wholesale[,i, with = FALSE], main = i)
 }
@@ -58,130 +60,147 @@ for (i in colnames(wholeLg)[-1:-2]) {
       boxplot(wholeLg[,i, with = FALSE], main = i)
 }
 
-# outlier stalker
+## outlier stalker
 detect_outliers <- function(x){
       upperBound <- median(x) + 1.5 * IQR(x)
       lowerBound <- median(x) - 1.5 * IQR(x)
       which(x > upperBound | x < lowerBound)
 }
 
-detect_outliers(wholeLg$Milk)
-
-## go get them
+## stalking outliers
 wholesale %>% 
       select(Fresh : Delicassen) %>% 
       map(detect_outliers) %>% 
-      unlist -> outliers
-
-outliers %>% table() %>%
+      unlist %>%
+      table() %>%
       as.data.frame() %>% 
       .[order(.$Freq, decreasing = T),] %T>%
-      View() -> outliers
-
-outliers %>% 
+      # View() %>% 
       filter(Freq > 4) %>%
       .[,1] %>% 
       as.numeric() -> outIndex
 
 wholeC <- wholeLg[-outIndex,]
-rm(outliers, outIndex, detect_outliers)
+rm(outIndex, detect_outliers)
 
 #--- 1.2 kMeans ----------------------------------------------------------------
-# elbow method
+set.seed(12345)
+
+## elbow estimation
 clusters <- c()
 for (i in 1:10) {
      clusters <- c(clusters, kmeans(wholeC[,-1:-2],i)$tot.withinss)
       
 }
+
 plot(1:i, clusters, type = "b")
+lines(c(2,10), clusters[c(2,10)], col = "green")
+lines(c(3,10), clusters[c(3,10)], col = "red")
+lines(c(4,10), clusters[c(4,10)], col = "blue")
 
-lines(c(3,10), clusters[c(3,10)], col = "green")
-lines(c(4,10), clusters[c(4,10)], col = "red")
-lines(c(5,10), clusters[c(5,10)], col = "blue")
-
-(clusters / clusters[1]) %>% subtract(lag(.)) * 100
-k <- 2
-
+(clusters / clusters[1]) %>% diff(1) * 100
 rm(i,clusters)
 
-# visualisation
+## "discussion about why to choose k = 2" 
+k <- 2
+
+## visualisation
 kModel <- kmeans(wholeC[,-1:-2],k)
-kModel$centers
 wholeC$clust <- kModel$cluster
 
 ggplot(wholeC, aes(x = Channel, y = Region, color = as.factor(clust))) +
       geom_jitter()
 
+## cluster explanations?
 for (i in 1:k) {
       print(wholeC[wholeC$clust == i,.(Channel, Region)] %>% table())
 }
 
 clusplot(wholeC, kModel$cluster, color = TRUE, shade = T, labels = 1)
 
-#--- 1.2 hClust ----------------------------------------------------------------
+#--- 1.3 hClust ----------------------------------------------------------------
 wholeMat <- dist(wholeC[,-1:-2])
 
-hModel <- hclust(wholeMat, method = "complete")
+# complete with squared distances!
+hModel <- hclust(wholeMat^2, method = "complete")
 plot(hModel)
 
+## find the longest line
 heights <-
       hModel$height %>% 
       .[order(.)]
       
-(diff(heights,lag = 1) %>% which.max() %>% add(1) %>% heights[.] -> h)
+(diff(heights,lag = 1) %>%
+            which.max() %>%
+            add(1) %>%
+            heights[.] -> h)
 
+## label groups
 rect.hclust(hModel, h = h)
 
-ggplot(wholeC, aes(x = Channel, y = Region, color = as.factor(hcl))) +
-      geom_jitter()
-
 groups <- cutree(hModel, h = h - 0.1)
+
+groups %>% table
+
+## visualisation 
+ggplot(wholeC, aes(x = Channel, y = Region, color = as.factor(groups))) +
+      geom_jitter()
 
 clusplot(wholeC[,-1:-2], groups , color = TRUE, 
          shade = T, labels = 1, lines = 0)
 
-groups %>% table
 
 #-- PART 2 - hclust & basket ##################################################
 purchase <- read_xlsx("w2/data/online_retail.xlsx")
+
+## overview
 str(purchase)
 summary(purchase)
 
+## remove unvalidated transactions
 purchase %<>% 
       filter(Quantity > 0, UnitPrice > 0, !is.na(CustomerID))
 
 summary(purchase)
 
+## item frequencies and "transation (client) table"
 itemFreq <- as.data.table(purchase)[,.(freq = .N), by = .(CustomerID, StockCode)]
-View(head(itemFreq, 500), "itemFreq")
-
+# View(head(itemFreq, 500), "itemFreq")
 custItem <- spread(itemFreq, StockCode, freq, fill = 0)
-
 # colnames(custItem)[1:10]
 rownames(custItem) <- custItem$CustomerID
 custItem$CustomerID <- NULL
-
 custItem <- as.matrix(custItem)
+
+## incidence matrix (otherwise squares)
 incidence <- custItem
 incidence[incidence > 0] <- 1
 
+## item x item factorisation
 items <- t(custItem) %*% incidence
 # View(items[1:10,1:10])
 
+## do not divide by zero!
 items[items == 0] <- 0.1
 items <- 1/items
+
+## same items are very close
 diag(items) <- 0
 
+## to lower the computation time
 itemSample <- items[1:500,1:500]
 
+## cluster items
 distMat <- dist(itemSample)
 tree <- hclust(distMat, method = "complete")
 
+## print out
 pdf("dendogram.pdf", width = 120, height = 75)
 plot(tree)
 abline(h = 40, col = "red")
 dev.off()
 
+## prepare the items desc and sales
 purchase %>% 
       # filter(StockCode %in% c("20667","21310","17028J","17001","20816")) %>% 
       filter(StockCode %in% c("20861", "16169P","17007B")) %>%
@@ -189,4 +208,3 @@ purchase %>%
       unique()
 
 as.data.table(purchase)[,.(Qty = sum(Quantity)), by = StockCode] -> itemSales
-
