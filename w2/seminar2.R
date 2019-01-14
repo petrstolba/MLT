196a9f6b6d0a901f93e5cdff95801e53aba42d3b
kubistmi <kubistmi@gmail.com>
2019-01-14 21:58:30 +0100

    Week 2 seminar reformatted + minor changes.

--- w2/seminar2.R
+++ w2/seminar2.R
@@ -1,10 +1,10 @@
 #####################
 ## Seminar 2       ##
 ## Michal Kubista  ##
-## 15 January 2018 ##
+## 15 January 2019 ##
 #####################
 
-install_and_load <- function(name, char = T){
+install_and_load = function(name, char = T){
     if (!require(name, character.only = char)) {
         install.packages(name)
     }
@@ -27,10 +27,10 @@ if (!dir.exists("w2/data")) {
 
 #--- 1.1 ETL -------------------------------------------------------------------
 # download data from 
-# https://drive.google.com/file/d/18QC-ZjT3pUTFWNgAl8G4VZgDA-DQkNni/view?usp=sharing
+# https://drive.google.com/open?id=1b6UZijHw-xN6dIPTq2RCu3WbmScHkg95
 # into w2/data
 
-prodTab <- fread("w2/data/prod_structure.csv")
+prodTab = fread("w2/data/prod_structure.csv")
 
 ## The first data overview
 str(prodTab)
@@ -57,30 +57,27 @@ map(prodTab, ~length(unique(.)))
     # classifier, we will split the product_names into three different
     # description columns
 
-inject <- function(x){
-      x %<>%
-            strsplit(split = " ") %>% 
-            unlist()
+inject = function(x){
+      x %>%
+        strsplit(split = " ") %>% 
+        unlist() %>% 
+        as.list()
 }
 
 ## splitting the product names
     # now let's apply the user-defined function over the rows of the table,
     # creating a new table and add the column names
-newCols <- apply(prodTab[,2], 1 ,inject) %>%
-      do.call(rbind.data.frame,.)
-colnames(newCols) <- c("desc1","desc2","desc3")
+prodTab[, c("desc1", "desc2", "desc3") := inject(product_name),
+        by = product_name]
 
-## binding together, all factors!
-    # now we will bind the original table with the new columns
-    # and change the column clases into factors
+## all factors!
+    # change the column clases into factors
     # as Bayes is not able to work with text data
-prodTab <- cbind(prodTab,newCols)
-prodTab <- apply(prodTab,2,as.factor) %>% as.data.frame()
+prodTab = map_df(prodTab,as.factor)
 
     # let's check the unique values in all columns
     # we have most of the unique descriptions in the second and third column
 map(prodTab, ~length(unique(.)))
-
 
 ## train & test division
     # because of the random splitting, we are setting the seed to ensure
@@ -88,9 +85,9 @@ map(prodTab, ~length(unique(.)))
     # test data
 set.seed(123)
 nrow(prodTab) %>% {sample(.,. * 0.50)} -> index
-train <- prodTab[index,]
-test <- prodTab[-index,]
-rm(newCols, index)
+train = prodTab[index,]
+test = prodTab[-index,]
+rm(index, inject)
 
 #--- 1.2 LABELLING -------------------------------------------------------------
 if (!require("e1071")) {
@@ -99,19 +96,24 @@ if (!require("e1071")) {
 }
 
 ## training
-bayes <- naiveBayes(category_name ~ ., train)
+bayes = naiveBayes(category_name ~ ., train)
+
+# in-sample accuracy?
+train %>% 
+    mutate(lab = predict(bayes, train)) %>% 
+    summarise(acc = sum(lab == category_name))
 
 ## prediction
-test$lab <- predict(bayes, test)
+test$lab = predict(bayes, test)
 
 ## changin the columns
     # changing the column order for the purpose ofo interpratation and
     # model performance assessment, putting the original and predicted
     # labels next to each other
     # the accuracy looks very well on the first peak
-test <- test[,c(1,6,2:5)]
+test = test[,c(1,6,2:5)]
 View(test)
-test$ok <- test$category_name == test$lab
+test$ok = test$category_name == test$lab
 
 ## accuracy statistics
     # defining the accuracy statistics:
@@ -122,9 +124,16 @@ test$ok <- test$category_name == test$lab
     #                   observations have higher number of bad predictions,
     #                   but there is no surprise in that either 
 sum(test$ok)/nrow(test) * 100
-table(test$category_name, test$lab)
 
-rm(bayes, prodTab, test, train, inject)
+table(test$category_name, test$lab) %>% 
+    print() %>% 
+    as.data.frame() %>% 
+    group_by(Var1) %>% 
+    summarise(ok = sum((Var1 == Var2)*Freq),
+              count = sum(Freq)) %>% 
+    mutate(acc = ok/count * 100)
+
+rm(bayes, prodTab, test, train)
 
 #-- PART 2 - APRIORI ###########################################################
 
@@ -132,20 +141,20 @@ rm(bayes, prodTab, test, train, inject)
 ##--- 2.1.1 ETL ----------------------------------------------------------------
 download.file("http://fimi.ua.ac.be/data/retail.dat.gz", "w2/data/retail.dat.gz")
 
-transRaw <- read.delim("w2/data/retail.dat.gz",
+transRaw = read.delim("w2/data/retail.dat.gz",
                 stringsAsFactors = FALSE)
 
-colnames(transRaw) <- "items"
+colnames(transRaw) = "items"
 
 transRaw %>% head(500) %>% View()
 
 ## find unique items
 strsplit(transRaw$items, split = " ") %>% 
       unlist() -> items
-itemsUn <- unique(items)
+itemsUn = unique(items)
 
 ## this will not work :/
-mat <- matrix(0, nrow(transRaw), length(itemsUn))
+mat = matrix(0, nrow(transRaw), length(itemsUn))
 
 ## item frequencies
 table(items) %>% 
@@ -166,28 +175,28 @@ itemsFreq %>%
 ##____transactions). 
 rm(items, itemsUn, itemsFreq)
 
-inject <- function(raw){
+inject = function(raw){
       raw %>%
             strsplit(split = " ") %>%
             unlist() -> nonList
       
-      index <- itemsCh %in% nonList %>% which()
-      out <- rep(0, length(itemsCh))
-      out[index] <- 1
+      index = itemsCh %in% nonList %>% which()
+      out = rep(0, length(itemsCh))
+      out[index] = 1
       return(out)
 }
 #
 
 # WHAT NOT TO DO! ----
-# transMat2 <- matrix(0,nrow(transRaw), length(itemsCh))
-# colnames(transMat2) <- itemsCh
-# timeFor <- system.time({
+# transMat2 = matrix(0,nrow(transRaw), length(itemsCh))
+# colnames(transMat2) = itemsCh
+# timeFor = system.time({
 #       for(i in seq_along(transRaw$items)){
 #             transRaw$items[1] %>%
 #                   strsplit(split = " ") %>%
 #                   unlist() -> non_list
-#             index <- itemsCh %in% non_list %>% which()
-#             transMat2[i,index] <- 1
+#             index = itemsCh %in% non_list %>% which()
+#             transMat2[i,index] = 1
 #
 #             print(paste(round(i/nrow(transRaw)*100,3),"%"))
 #             flush.console()
@@ -196,10 +205,10 @@ inject <- function(raw){
 # rm(transMat2, i , index, non_list, timeFor, timeApply)
 # ----
 
-system.time({transMat <- t(sapply(transRaw$items, inject))})
+system.time({transMat = t(sapply(transRaw$items, inject))})
 
-colnames(transMat) <- itemsCh
-rownames(transMat) <- 1:nrow(transMat)
+colnames(transMat) = itemsCh
+rownames(transMat) = 1:nrow(transMat)
 
 ## are the dimensions ok?
 dim(transMat) == c(nrow(transRaw), length(itemsCh))
@@ -207,45 +216,45 @@ dim(transMat) == c(nrow(transRaw), length(itemsCh))
 rm(transRaw, itemsCh, inject)
 
 ##--- 2.1.2 ASSOCIATIONS -------------------------------------------------------
-model <- apriori(transMat, parameter = list(support = 0.01, confidence = 0.5))
+model = apriori(transMat, parameter = list(support = 0.01, confidence = 0.5))
 
 inspect(model) %>%
       as.data.frame() -> ruleTab
 
 plot(model)
-plot(model, interactive = TRUE)
+plot(model, engine = 'interactive')
 
 rm(model, ruleTab, transMat)
 
 #--- 2.2 DATAFRAME INPUT -------------------------------------------------------
 ##--- 2.2.1 ETL ----------------------------------------------------------------
-url <- "https://archive.ics.uci.edu/ml/machine-learning-databases/00352/Online%20Retail.xlsx" 
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00352/Online%20Retail.xlsx" 
 
 download.file(url, "w2/data/online_retail.xlsx", mode = 'wb' )
 
-# transRaw <- read_xlsx("w2/data/online_retail.xlsx")
 # because of the problems with excel reading, after downloading the file
 # from Gdrive, run following
-# if you read the csv start from line 262 (sparseMatrix)
-transRaw <- fread("w2/data/online.csv")
+# !!!!! if you read the csv start from line 262  !!!!
+transRaw = fread("w2/data/online.csv")
 
+transRaw = read_xlsx("w2/data/online_retail.xlsx")
 str(transRaw)
 
 transRaw %<>% 
       select(InvoiceNo, Description)
 
 ## create a product table
-prodUn <- transRaw$Description %>% unique()
+prodUn = transRaw$Description %>% unique()
 
-prodTable <- cbind(Description = prodUn,
+prodTable = cbind(Description = prodUn,
                    prodID = seq_along(prodUn)) %>%
       as.data.frame(stringsAsFactors = F)
 rm(prodUn)
 
 ## create a transaction table 
-transUn <- transRaw$InvoiceNo %>% unique()
+transUn = transRaw$InvoiceNo %>% unique()
 
-transTable <-  cbind(InvoiceNo = transUn,
+transTable =  cbind(InvoiceNo = transUn,
                      transID = seq_along(transUn)) %>%
       as.data.frame(stringsAsFactors = F)
 rm(transUn)
@@ -256,28 +265,28 @@ transRaw %<>%
       right_join(transTable, by = "InvoiceNo")
 
 ## IDs as numeric
-transRaw[,3:4] <- apply(transRaw[,3:4],2,as.numeric)
+transRaw[,3:4] = apply(transRaw[,3:4],2,as.numeric)
 
 ## create sparse matrix based on IDs
 
-transMat <- sparseMatrix(j = transRaw$transID,
+transMat = sparseMatrix(j = transRaw$transID,
                          i = transRaw$prodID)
 
 
-rownames(transMat) <- prodTable$Description
-colnames(transMat) <- transTable$InvoiceNo
+rownames(transMat) = prodTable$Description
+colnames(transMat) = transTable$InvoiceNo
 
 ##--- 2.2.2 ASSOCIATIONS -------------------------------------------------------
-model <- apriori(transMat, parameter = list(support = 0.02, confidence = 0.25))
+model = apriori(transMat, parameter = list(support = 0.02, confidence = 0.25))
 
 model %>% inspect() %>% as.data.frame()
 
 ## extract data manually
-rules <- cbind(labels = labels(model), model@quality)
+rules = cbind(labels = labels(model), model@quality)
     # "@" because S4 happened
 
-rules$lhs <- gsub("=>.*","", rules$labels)
-rules$rhs <- gsub(".*=>","", rules$labels)
+rules$lhs = gsub("=>.*","", rules$labels)
+rules$rhs = gsub(".*=>","", rules$labels)
 
-rules <- rules[,c("lhs","rhs","support","confidence","lift", "count")]
+rules = rules[,c("lhs","rhs","support","confidence","lift", "count")]
 View(rules)
