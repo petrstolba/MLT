35cc798d4f104467d82f45b9d684d35b6a43ca57
Valentin Artemev <valentin.artemev@cerge-ei.cz>
2020-02-17 13:33:59 +0100

    W6 seminar: some comments and install_and_load function

--- w6/seminar6.R
+++ w6/seminar6.R
@@ -4,16 +4,25 @@
 ## 17 February 2020##
 #####################
 
+install_and_load = function(name, char = T){
+  if (!require(name, character.only = char)) {
+    install.packages(name)
+  }
+  require(name, character.only = char)
+}
+
 sapply(
   c("data.table","tidyverse","magrittr","readxl"),
-  require,
-  character.only = T
+  install_and_load
 )
 
 #-- PART 1 - SIMPLE PRICINGS ###################################################
 #--- 1.1 ETL -------------------------------------------------------------------
+# download data from 
+# https://drive.google.com/open?id=1b6UZijHw-xN6dIPTq2RCu3WbmScHkg95
+# into w2/data
 transRaw =
-    as.data.table(read_xlsx("w2/data/online_retail.xlsx"))
+  as.data.table(read_xlsx("w2/data/online_retail.xlsx"))
 
 summary(transRaw)
 
@@ -22,7 +31,7 @@ transRaw = transRaw[Quantity > 0 & UnitPrice > 0]
 
 ## items with more (or equal to) 6 different prices
 transRaw[,.(count = length(unique(UnitPrice))), by = "StockCode"
-        ][order(count, decreasing = TRUE)
+         ][order(count, decreasing = TRUE)
            ][count >= 6, StockCode
              ] %>% 
   .[-1:-3] -> prodID
@@ -34,8 +43,8 @@ trans$InvoiceDate %<>% as.Date
 ## create elasticity table
 ## this runs ~ 601 linear models btw :D
 elas_table = trans[,
-                    .(elas = lm(log(Quantity)~log(UnitPrice))$coefficients[2]),
-                    by = StockCode
+                   .(elas = lm(log(Quantity)~log(UnitPrice))$coefficients[2]),
+                   by = StockCode
                    ][order(elas, decreasing = T)]
 
 elas_table$elas %>% summary()
@@ -45,25 +54,25 @@ elas_table$elas %>% summary()
 elas_table = elas_table[elas < 0]
 
 elas_table$elasG = cut(elas_table$elas,
-                      breaks = c(-14, -2, -1, 0),
-                      labels = c("H", "M", "L"))
+                       breaks = c(-14, -2, -1, 0),
+                       labels = c("H", "M", "L"))
 
 summary(elas_table$elasG)
 
 ## function to find price index of the product relative to usual price
 find_price_index = function(code, day) {
-
-    trans %>% 
-        filter(StockCode == code) %>% 
-        select(StockCode, InvoiceDate, UnitPrice) -> price
-    
-    price %>% 
-        filter(InvoiceDate == day) %>% 
-        .$UnitPrice %>% 
-        mean(na.rm = T) -> target
-    
-    mean_price = median(price$UnitPrice, na.rm = TRUE)
-    return(target / mean_price)
+  
+  trans %>% 
+    filter(StockCode == code) %>% 
+    select(StockCode, InvoiceDate, UnitPrice) -> price
+  
+  price %>% 
+    filter(InvoiceDate == day) %>% 
+    .$UnitPrice %>% 
+    mean(na.rm = T) -> target
+  
+  mean_price = median(price$UnitPrice, na.rm = TRUE)
+  return(target / mean_price)
 }
 
 ## find price indexes for given day
@@ -80,16 +89,16 @@ elas_table[pi_table, on = "StockCode"][!is.na(elas)] -> pie_table
 
 ## ordered factors for visualisation
 pie_table %<>% 
-    mutate(group = paste0(piG, elasG)) %>% 
-    mutate(piG = ordered(piG, c("L","M","H"))) %>% 
-    mutate(elasG = ordered(elasG, c("L","M","H")))
+  mutate(group = paste0(piG, elasG)) %>% 
+  mutate(piG = ordered(piG, c("L","M","H"))) %>% 
+  mutate(elasG = ordered(elasG, c("L","M","H")))
 
 ggplot(pie_table, aes(x = piG, y = elasG, color = group)) +
-    geom_jitter() +
-    geom_hline(yintercept = 1.5) +
-    geom_hline(yintercept = 2.5) +
-    geom_vline(xintercept = 1.5) +
-    geom_vline(xintercept = 2.5)
+  geom_jitter() +
+  geom_hline(yintercept = 1.5) +
+  geom_hline(yintercept = 2.5) +
+  geom_vline(xintercept = 1.5) +
+  geom_vline(xintercept = 2.5)
 
 #--- 1.2 PENE TABLE -------------------------------------------------------------
 ## filter SKUs with high elasticity and high price index
@@ -106,14 +115,14 @@ trans[base, on = "StockCode"
 ## create pene table
 ## runs ~ 114 linear models
 pene_table = trans[StockCode %in% hh_items,
-                    .(elas = lm(log(Quantity)~log(UnitPrice))$coefficients[2]),
-                    by = .(StockCode, promo)
-                    ][order(elas, decreasing = T)]
+                   .(elas = lm(log(Quantity)~log(UnitPrice))$coefficients[2]),
+                   by = .(StockCode, promo)
+                   ][order(elas, decreasing = T)]
 
 ## long to wide
 pene_table %>% 
-    dcast(StockCode~promo) %>% 
-    .[complete.cases(.)] -> pene_table
+  dcast(StockCode~promo) %>% 
+  .[complete.cases(.)] -> pene_table
 
 setnames(pene_table, c("0","1"), c("N","P"))
 
@@ -121,26 +130,27 @@ setnames(pene_table, c("0","1"), c("N","P"))
 pene_table$PG = ordered(ifelse(pene_table$P < -1.7, "H", "L"), c("L", "H"))
 pene_table$NG = ordered(ifelse(pene_table$N < -1.2, "H", "L"), c("L", "H"))
 pene_table %<>% 
-    mutate(group = paste0(PG, NG))
+  mutate(group = paste0(PG, NG))
 
 ggplot(pene_table, aes(x = PG, y = NG, color = group)) +
-    geom_jitter() +
-    geom_hline(yintercept = 1.5) +
-    geom_vline(xintercept = 1.5)
-    
+  geom_jitter() +
+  geom_hline(yintercept = 1.5) +
+  geom_vline(xintercept = 1.5)
+
 pene_table %>% 
-    filter(PG == "L" & NG == "L")
+  filter(PG == "L" & NG == "L")
 
 pie_table %>% filter(StockCode == "22608")
 
 trans %>% 
-    filter(StockCode == "22608") -> SKU
+  filter(StockCode == "22608") -> SKU
 
 ggplot(SKU, aes(x = UnitPrice, y = Quantity, color = as.factor(promo))) +
-    geom_line() +
-    geom_point() +
-    geom_smooth(method = 'lm')
+  geom_line() +
+  geom_point() +
+  geom_smooth(method = 'lm')
 
 ggplot(SKU, aes(x = InvoiceDate, y = UnitPrice, color = as.factor(promo))) +
-    geom_line() +
-    geom_point()
+  geom_line() +
+  geom_point()
+
